0cc274794f6ad5f82297e2a2e5e47f90663acdc4
ZeyadMounir <zeyad82@yahoo.com>
2025-01-21 11:21:57 +0200

    added volume config

---
+++ config/volume.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+   'telegram' => [
+        'token' => env('TG_TOKEN'),
+        'volumes_chat_id' => env('VOLUME_CHAT_ID'),
+        'emas_chat_id' => env('EMAS_CHAT_ID'),
+        'rsi_chat_id' => env('RSI_CHAT_ID'),
+   ]
+
+];
